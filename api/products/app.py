634264fce846fde7d643653ada0e09788804edff
Madhusudhan M <madhusudhan.m@reward360.co>
2026-08-05 11:52:17 +0530

    Phase-4 completed-Products

---
+++ api/products/app.py
@@ -0,0 +1,63 @@
+from fastapi import APIRouter
+
+from api.products.schema import (
+    CreateProductSchema,
+    UpdateProductSchema
+)
+
+from api.products import utility
+
+router = APIRouter()
+
+@router.post("/products")
+async def create_product(
+    data: CreateProductSchema
+):
+    return await utility.create_product(
+        data.model_dump()
+    )
+
+
+@router.get("/products")
+async def get_products():
+    return await utility.get_products()
+
+
+@router.get("/products/{product_id}")
+async def get_product(
+    product_id: str
+):
+    return await utility.get_product(
+        product_id
+    )
+
+
+@router.get(
+    "/products/account/{account_id}"
+)
+async def get_products_by_account(
+    account_id: str
+):
+    return await utility.get_products_by_account(
+        account_id
+    )
+
+
+@router.put(
+    "/products/{product_id}"
+)
+async def update_product(
+    product_id: str,
+    data: UpdateProductSchema
+):
+
+    payload = {
+        k: v
+        for k, v in data.model_dump().items()
+        if v is not None
+    }
+
+    return await utility.update_product(
+        product_id,
+        payload
+    )
--- main.py
+++ main.py
@@ -10,8 +10,17 @@ from api.accounts.app import (
     router as account_router
 )
 
+from api.products.app import (
+    router as product_router
+)
+
 app = FastAPI(
     title="Campaign CMS Clone"
+)
+
+app.include_router(
+    product_router,
+    tags=["Products"]
 )
 
 app.include_router(
